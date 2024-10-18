c3c276ea6bcff72a85310069e9b7130161c83656
Yanmei Zhang <67971868+Yanmei-Zhang@users.noreply.github.com>
2024-10-18 15:52:43 -0500

    Update asv_16s_phyloseq.R

--- Script/asv_16s_phyloseq.R
+++ Script/asv_16s_phyloseq.R
@@ -36,24 +36,4 @@ taxa_names(ps.16s) <- paste0("ASV", seq(ntaxa(ps.16s)))
 rep.seq <- refseq(ps.16s)
 Biostrings::writeXStringSet(rep.seq,  file = "results/rep_seqs.fasta", format = "fasta")
 
-# Construct phylogenetic tree
-# sequence alignment
-mult <- msa(rep.seq, method="ClustalW", type="dna", order="input")
-# build tree using package phangorn
-phang.align <- as.phyDat(mult, type="DNA")
-dm <- dist.ml(phang.align)
-treeNJ <- NJ(dm) # Note, tip order != sequence order
-fit = pml(treeNJ, data=phang.align)
-
-## negative edges length changed to 0!
-fitGTR <- update(fit, k=4, inv=0.2)
-fitGTR <- optim.pml(fitGTR, model="GTR", optInv=TRUE, optGamma=TRUE,
-                    rearrangement = "stochastic", control = pml.control(trace = 0))
-detach("package:phangorn", unload=TRUE)
-
-# Merge phylo tree into phyloseq
-ps.16s <- merge_phyloseq(ps.16s, phy_tree(fitGTR$tree))
-
-save.image(file="asv_16s_phyloseq.RData")
-saveRDS(ps.16s, "results/ps.16s.rds")
 
